8c153f16246bd4dfeda6ed83af1e310fcdd2571f
GOLAM KIBRIA TUHIN <golam.kibria@sonaliintellect.com>
2024-10-29 14:54:38 +0600

    initial commit

---
+++ src/main/java/com/notes_api_service/NotesApiServiceApplication.java
@@ -0,0 +1,13 @@
+package com.notes_api_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesApiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesApiServiceApplication.class, args);
+	}
+
+}
