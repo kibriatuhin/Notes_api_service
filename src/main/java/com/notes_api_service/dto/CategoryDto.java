6926a6e74bc9ce710335bf50409f13511e1e084e
Golam Kibria Tuhin <75140956+kibriatuhin@users.noreply.github.com>
2024-11-04 16:59:21 +0600

    implement DTO pattern for category module

---
+++ src/main/java/com/notes_api_service/dto/CategoryDto.java
@@ -0,0 +1,22 @@
+package com.notes_api_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDto {
+    private Integer id;
+    private String name;
+    private String description;
+    private Boolean isActive;
+    private Integer createdBy;
+    private Date createdOn;
+    private Integer updatedBy;
+    private Date updatedOn;
+}
